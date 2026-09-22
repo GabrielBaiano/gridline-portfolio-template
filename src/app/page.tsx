ceb6a95cf49582d9334889bf3224b1adda049329
GabrielBaiano <gabrielngama@gmail.com>
2026-09-22 08:03:53 -0300

    chore: revert bottom banner to clean dot grid, update tab favicon to square-ended asterisk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { HeaderBanner } from "@/components/HeaderBanner";
 import { DotGridSnakeGame } from "@/components/DotGridSnakeGame";
-import { MountainDotGrid } from "@/components/MountainDotGrid";
 import { StickyNav } from "@/components/StickyNav";
 import { HeroSection } from "@/components/HeroSection";
 import { ActivityCalendar } from "@/components/ActivityCalendar";
@@ -63,9 +62,9 @@ export default function Home() {
 
       <div className="divider-dashed"></div>
 
-      {/* Bottom Dot Grid Banner with Halftone Mountain Relief */}
+      {/* Bottom Dot Grid Banner */}
       <div className="max-w-[690px] mx-2 sm:mx-8 md:mx-auto relative p-3 flex flex-col container-dashed">
-        <MountainDotGrid className="w-full sm:min-h-[220px] min-h-[140px] h-full grow" />
+        <DotGridSnakeGame className="w-full sm:min-h-[220px] min-h-[120px] h-full grow" />
       </div>
 
       {/* Scroll to Top Floating Button */}
