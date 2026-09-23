e270a9e7fd8eb28e3c7b5dd5b8fb62938dddb888
GabrielBaiano <gabrielngama@gmail.com>
2026-09-23 07:57:05 -0300

    fix(layout): remove flex-col wrapper on slug pages to align subpage nav container

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -94,7 +94,7 @@ export default async function ProjectDetailPage({ params }: ProjectPageProps) {
   );
 
   return (
-    <div className="min-h-screen bg-background flex flex-col">
+    <div className="min-h-screen bg-background">
       {/* Top Dot-Grid Banner */}
       <div className="relative z-50 bg-background">
         <div className="max-w-[690px] mx-2 sm:mx-8 md:mx-auto relative p-3 flex flex-col container-dashed">
@@ -240,7 +240,7 @@ export default async function ProjectDetailPage({ params }: ProjectPageProps) {
       <div className="divider-dashed" />
 
       {/* Bottom Dot-Grid Banner */}
-      <div className="max-w-[690px] mx-2 sm:mx-8 md:mx-auto relative p-3 flex flex-col flex-1 md:w-full container-dashed">
+      <div className="max-w-[690px] mx-2 sm:mx-8 md:mx-auto relative p-3 flex flex-col container-dashed">
         <div className="w-full sm:min-h-[220px] min-h-[100px] h-full grow bg-dot-grid rounded-[4px]" />
       </div>
 
